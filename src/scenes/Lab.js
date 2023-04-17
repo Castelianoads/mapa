3b9a03caa3da9d5fa60fa4178d4c11419fb02f72
Carlos Eduardo Casteliano de Paula <carlosppaula2008@gmail.com>
2023-04-17 19:56:52 -0300

    Include projeto

---
+++ src/scenes/Lab.js
@@ -0,0 +1,19 @@
+import { Scene } from "phaser";
+
+export default class Lab extends Scene {
+  constructor(){
+    super('Lab');
+  }
+
+  preload(){
+
+  }
+
+  create(){
+
+  }
+
+  update(){
+
+  }
+}
