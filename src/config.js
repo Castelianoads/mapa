224098da49f51245a14be6a76ce40b524b1a55fa
Carlos Eduardo Casteliano de Paula <carlosppaula2008@gmail.com>
2023-04-29 16:06:09 -0300

    Alteração do tamanho do jogo

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const TILE_SIZE = 16 // Tamanho do mapa em px
 export const CONFIG = {
   TILE_SIZE: TILE_SIZE,
   GAME_WIDTH: 10 * TILE_SIZE,
-  GAME_HEIGHT: 28 * TILE_SIZE,
+  GAME_HEIGHT: 29 * TILE_SIZE,
   GAME_SCALE: 2,
   DEBUG_COLLISION: false
 }
